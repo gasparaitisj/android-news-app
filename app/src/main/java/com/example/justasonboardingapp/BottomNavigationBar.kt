fca07f4fc4fa91e605165910e1c5b38e19a258bb
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-09-21 10:49:59 +0300

    Add BottomNavigationBar.kt and BottomNavItem.kt

---
+++ app/src/main/java/com/example/justasonboardingapp/BottomNavigationBar.kt
@@ -0,0 +1,57 @@
+package com.example.justasonboardingapp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.justasonboardingapp.ui.theme.Roboto
+
+@ExperimentalMaterialApi
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = colorResource(id = R.color.botNavBar),
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = colorResource(id = R.color.selectedContent),
+                unselectedContentColor = colorResource(id = R.color.unselectedContent),
+                alwaysShowLabel = false,
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        Icon(
+                            imageVector = if (selected) item.iconActive else item.iconInactive,
+                            contentDescription = item.name
+                        )
+                    }
+                },
+                label = {
+                    Text(
+                        text = item.name,
+                        textAlign = TextAlign.Center,
+                        fontSize = 12.sp,
+                        fontFamily = Roboto
+                    )
+                }
+            )
+        }
+    }
+}
