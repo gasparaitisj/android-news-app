26dda0659b8173d64009879e714eaf21cdcefefb
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-09-23 14:31:26 +0300

    Add Top App Bar to AboutScreen.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/about/AboutScreen.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/about/AboutScreen.kt
@@ -1,18 +1,39 @@
 package com.telesoftas.justasonboardingapp.about
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.telesoftas.justasonboardingapp.R
 
 @Composable
 fun AboutScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "About screen")
-    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("About")
+                },
+                backgroundColor = colorResource(id = R.color.topAppBarBackground),
+                contentColor = colorResource(id = R.color.topAppBarContent)
+            )
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(bottom = paddingValues.calculateBottomPadding())
+            ) {}
+        }
+    )
+}
+
+@Preview
+@Composable
+fun AboutScreenPreview() {
+    AboutScreen()
 }
