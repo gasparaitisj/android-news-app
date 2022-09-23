ff2590f86f12f9123ac9545e65b2d8595f9ddc46
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-09-23 17:06:22 +0300

    Replace placeholder images and text in TutorialScreen.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/tutorial/OnBoardingPage.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/tutorial/OnBoardingPage.kt
@@ -1,28 +1,29 @@
 package com.telesoftas.justasonboardingapp.tutorial
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import com.telesoftas.justasonboardingapp.R
 
 sealed class OnBoardingPage(
     @DrawableRes val image: Int,
-    val title: String,
-    val description: String
+    @StringRes val titleResId: Int,
+    @StringRes val descriptionResId: Int
 ) {
     object First : OnBoardingPage(
-        image = R.drawable.btn_about,
-        title = "Meeting",
-        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+        image = R.drawable.tutorial_first_image,
+        titleResId = R.string.tutorial_screen_first_title,
+        descriptionResId = R.string.tutorial_screen_first_description
     )
 
     object Second : OnBoardingPage(
-        image = R.drawable.btn_favorite,
-        title = "Coordination",
-        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+        image = R.drawable.tutorial_second_image,
+        titleResId = R.string.tutorial_screen_second_title,
+        descriptionResId = R.string.tutorial_screen_second_description
     )
 
     object Third : OnBoardingPage(
-        image = R.drawable.btn_source_list,
-        title = "Dialogue",
-        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+        image = R.drawable.tutorial_third_image,
+        titleResId = R.string.tutorial_screen_third_title,
+        descriptionResId = R.string.tutorial_screen_third_description
     )
 }
--- app/src/main/java/com/telesoftas/justasonboardingapp/tutorial/TutorialScreen.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/tutorial/TutorialScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -73,7 +74,7 @@ fun PagerScreen(onBoardingPage: OnBoardingPage) {
         Text(
             modifier = Modifier
                 .fillMaxWidth(),
-            text = onBoardingPage.title,
+            text = stringResource(onBoardingPage.titleResId),
             fontSize = Typography.h4.fontSize,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center
@@ -83,7 +84,7 @@ fun PagerScreen(onBoardingPage: OnBoardingPage) {
                 .fillMaxWidth()
                 .padding(horizontal = 40.dp)
                 .padding(top = 20.dp),
-            text = onBoardingPage.description,
+            text = stringResource(id = onBoardingPage.descriptionResId),
             fontSize = MaterialTheme.typography.subtitle1.fontSize,
             fontWeight = FontWeight.Medium,
             textAlign = TextAlign.Center
