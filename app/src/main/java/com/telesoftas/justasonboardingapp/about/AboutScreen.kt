fa78c8541336512f7ab791b49c5bb52281e8af75
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-09-23 14:51:06 +0300

    Add unformatted text to AboutScreen.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/about/AboutScreen.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/about/AboutScreen.kt
@@ -8,8 +8,10 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.telesoftas.justasonboardingapp.R
+import com.telesoftas.justasonboardingapp.ui.theme.Typography
 
 @Composable
 fun AboutScreen() {
@@ -26,8 +28,29 @@ fun AboutScreen() {
         content = { paddingValues ->
             Column(
                 modifier = Modifier
-                    .padding(bottom = paddingValues.calculateBottomPadding())
-            ) {}
+                    .padding(bottom = paddingValues.calculateTopPadding())
+            ) {
+                Text(
+                    text = stringResource(id = R.string.about_screen_paragraph_1),
+                    style = Typography.subtitle2
+                )
+                Text(
+                    text = stringResource(id = R.string.about_screen_paragraph_2),
+                    style = Typography.body2
+                )
+                Text(
+                    text = stringResource(id = R.string.about_screen_paragraph_3),
+                    style = Typography.body2
+                )
+                Text(
+                    text = stringResource(id = R.string.about_screen_paragraph_4),
+                    style = Typography.subtitle2
+                )
+                Text(
+                    text = stringResource(id = R.string.about_screen_paragraph_5),
+                    style = Typography.body2
+                )
+            }
         }
     )
 }
