8dc8499d8d4ac4b59243070c39cae4c823a65ec3
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-09-28 14:22:03 +0300

    Add ArticlesApi.kt, ArticlesRepository.kt, NetworkModule.kt

---
+++ app/src/main/java/com/telesoftas/justasonboardingapp/utils/network/ArticlesApi.kt
@@ -0,0 +1,22 @@
+package com.telesoftas.justasonboardingapp.utils.network
+
+import com.telesoftas.justasonboardingapp.utils.network.data.ArticleCategory
+import com.telesoftas.justasonboardingapp.utils.network.data.GetArticlesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ArticlesApi {
+    @Headers("Content-Type: application/json")
+    @GET("articles")
+    suspend fun getArticles(
+        @Query("q") query: String? = null,
+        @Query("page") page: Int? = null,
+        @Query("pageSize") pageSize: Int? = null,
+        @Query("category") category: ArticleCategory? = null,
+        @Query("sortBy") sortBy: String? = null,
+        @Query("order") pageNumber: Int? = null,
+        @Query("x-request-id") xRequestId: String? = null
+    ): Response<GetArticlesResponse>
+}
