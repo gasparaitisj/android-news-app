51c1fbbe0093b5ab62dd56c661ed465f0c28bfbf
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-09-28 14:22:40 +0300

    Implement network request in SourceListScreen.kt and SourceListViewModel.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/sourcelist/SourceListScreen.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/sourcelist/SourceListScreen.kt
@@ -1,25 +1,66 @@
 package com.telesoftas.justasonboardingapp.sourcelist
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.telesoftas.justasonboardingapp.R
 import com.telesoftas.justasonboardingapp.ui.theme.Typography
+import com.telesoftas.justasonboardingapp.utils.network.Resource
+import com.telesoftas.justasonboardingapp.utils.network.Status
 
+@ExperimentalLifecycleComposeApi
 @Composable
-fun SourceListScreen(navController: NavHostController) {
+fun SourceListScreen(
+    navController: NavHostController,
+    viewModel: SourceListViewModel = hiltViewModel()
+) {
+    viewModel.getArticles()
+    val response by viewModel.response.collectAsStateWithLifecycle(initialValue = Resource.loading())
+    val newsSourceList = mutableListOf<NewsSource>()
+    when (response.status) {
+        Status.SUCCESS -> {
+            response.data?.articles?.let { articles ->
+                articles.forEach { article ->
+                    newsSourceList.add(NewsSource(
+                        article.title ?: "",
+                        article.description ?: ""
+                    ))
+                    println("category: " + article.category)
+                }
+            }
+        }
+        Status.LOADING -> {
+            // do nothing
+        }
+        Status.ERROR -> {
+            Toast.makeText(
+                LocalContext.current,
+                stringResource(id = R.string.network_error),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     Scaffold(
         content = { paddingValues ->
             Column(
                 modifier = Modifier.padding(top = paddingValues.calculateTopPadding()),
                 content = {
-                    SourceListContent(newsSourceList = emptyList())
+                    SourceListContent(newsSourceList = newsSourceList)
                 }
             )
         }
@@ -41,7 +82,9 @@ private fun SourceListContent(newsSourceList: List<NewsSource>) {
 
 @Composable
 private fun SourceItem(item: NewsSource) {
-    Column(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
         Text(text = item.title, style = Typography.h6)
         Spacer(modifier = Modifier.height(8.dp))
         Text(text = item.description, style = Typography.body2)
---
+++ app/src/main/java/com/telesoftas/justasonboardingapp/sourcelist/SourceListViewModel.kt
@@ -0,0 +1,44 @@
+package com.telesoftas.justasonboardingapp.sourcelist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.telesoftas.justasonboardingapp.utils.network.Resource
+import com.telesoftas.justasonboardingapp.utils.network.data.ArticleCategory
+import com.telesoftas.justasonboardingapp.utils.network.data.GetArticlesResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SourceListViewModel @Inject constructor(
+    private val articlesRepository: ArticlesRepository
+) : ViewModel() {
+    private val _response: MutableStateFlow<Resource<GetArticlesResponse>> =
+        MutableStateFlow(Resource.loading())
+    val response: StateFlow<Resource<GetArticlesResponse>> = _response.asStateFlow()
+
+    fun getArticles(
+        query: String? = null,
+        page: Int? = null,
+        pageSize: Int? = null,
+        category: ArticleCategory? = null,
+        sortBy: String? = null,
+        pageNumber: Int? = null,
+        xRequestId: String? = null
+    ) {
+        viewModelScope.launch {
+            _response.value = articlesRepository.getArticles(
+                query = query,
+                page = page,
+                pageSize = pageSize,
+                category = category,
+                sortBy = sortBy,
+                pageNumber = pageNumber,
+                xRequestId = xRequestId
+            )
+        }
+    }
+}
