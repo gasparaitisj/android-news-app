55fe95cc0e8f5498818ea272d9e70483b0b8ba16
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-09-28 07:46:48 +0300

    Implement SourceListScreen.kt view, add NewsSource.kt

---
+++ app/src/main/java/com/telesoftas/justasonboardingapp/sourcelist/NewsSource.kt
@@ -0,0 +1,6 @@
+package com.telesoftas.justasonboardingapp.sourcelist
+
+data class NewsSource(
+    val title: String,
+    val description: String
+)
--- app/src/main/java/com/telesoftas/justasonboardingapp/sourcelist/SourceListScreen.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/sourcelist/SourceListScreen.kt
@@ -1,15 +1,17 @@
 package com.telesoftas.justasonboardingapp.sourcelist
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.telesoftas.justasonboardingapp.ui.theme.Typography
 
-@ExperimentalMaterialApi
 @Composable
 fun SourceListScreen(navController: NavHostController) {
     Scaffold(
@@ -17,9 +19,41 @@ fun SourceListScreen(navController: NavHostController) {
             Column(
                 modifier = Modifier.padding(top = paddingValues.calculateTopPadding()),
                 content = {
-                    Text(text = "Source list screen")
+                    SourceListContent(newsSourceList = emptyList())
                 }
             )
         }
     )
+}
+
+@Composable
+private fun SourceListContent(newsSourceList: List<NewsSource>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        items(newsSourceList) { item ->
+            SourceItem(item = item)
+        }
+    }
+}
+
+@Composable
+private fun SourceItem(item: NewsSource) {
+    Column(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
+        Text(text = item.title, style = Typography.h6)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = item.description, style = Typography.body2)
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+private fun SourceListContentPreview() {
+    val list = listOf(
+        NewsSource("Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc"),
+        NewsSource("Lorem ipsum dolor sit amet", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc"),
+    )
+    SourceListContent(newsSourceList = list)
 }
