13c7fe5506ce6d896a38ef99ae837f67fbc5bcbb
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-04 09:07:38 +0300

    Add Timber to MainApplication.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/MainApplication.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/MainApplication.kt
@@ -2,6 +2,18 @@ package com.telesoftas.justasonboardingapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(CustomTimberDebugTree("JUDICIALGRANITE"))
+    }
+}
+
+class CustomTimberDebugTree(private val tagPrefix: String) : Timber.DebugTree() {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        super.log(priority, "$tagPrefix.$tag", message, t)
+    }
+}
