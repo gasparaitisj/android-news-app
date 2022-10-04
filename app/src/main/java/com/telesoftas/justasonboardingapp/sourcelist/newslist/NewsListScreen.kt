30990cf163fa789ccab565c2a0df36648b36ded1
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-04 09:09:17 +0300

    Add NewsListScreen.kt

---
+++ app/src/main/java/com/telesoftas/justasonboardingapp/sourcelist/newslist/NewsListScreen.kt
@@ -0,0 +1,29 @@
+package com.telesoftas.justasonboardingapp.sourcelist.newslist
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+
+@ExperimentalMaterialApi
+@Composable
+fun NewsListScreen(
+    navController: NavHostController,
+    viewModel: NewsListViewModel = hiltViewModel()
+) {
+    Scaffold(
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier.padding(paddingValues),
+                content = {
+                    Text(text = "News list screen")
+                }
+            )
+        }
+    )
+}
