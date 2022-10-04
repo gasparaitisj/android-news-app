e80dccc72ec523e0c7e260c1d9753784c6e6dcfa
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-04 16:03:05 +0300

    Add Article.kt

---
+++ app/src/main/java/com/telesoftas/justasonboardingapp/sourcelist/newslist/Article.kt
@@ -0,0 +1,14 @@
+package com.telesoftas.justasonboardingapp.sourcelist.newslist
+
+import com.telesoftas.justasonboardingapp.utils.network.data.ArticleCategory
+
+data class Article(
+    val id: String,
+    val publishedAt: String,
+    val source: String? = null,
+    val category: ArticleCategory,
+    val author: String? = null,
+    val title: String? = null,
+    val description: String? = null,
+    val imageURL: String? = null
+)
