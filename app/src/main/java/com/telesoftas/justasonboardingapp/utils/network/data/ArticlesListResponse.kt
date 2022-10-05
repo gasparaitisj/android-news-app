d2fe99d180518a1d4e27ef744ee5b206dff81f4b
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-05 11:15:01 +0300

    Add @Json annotations in ArticlesListResponse.kt, ArticlesPreviewResponse.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/utils/network/data/ArticlesListResponse.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/utils/network/data/ArticlesListResponse.kt
@@ -1,9 +1,20 @@
 package com.telesoftas.justasonboardingapp.utils.network.data
 
+import com.squareup.moshi.Json
+
 data class ArticlesListResponse (
+    @Json(name="totalArticles")
     val totalArticles: Long,
+
+    @Json(name="currentPage")
     val currentPage: Long,
+
+    @Json(name="totalPages")
     val totalPages: Long,
+
+    @Json(name="pageSize")
     val pageSize: Long,
+
+    @Json(name="articles")
     val articles: List<ArticlePreviewResponse>?
 )
