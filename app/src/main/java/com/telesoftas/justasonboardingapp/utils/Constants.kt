e3431046f5f2be6164710a46a067f1f7e950390a
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-10 11:36:18 +0300

    Update Constants.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/utils/Constants.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.telesoftas.justasonboardingapp.utils
 
 object Constants {
+    const val DATABASE_NAME = "database"
     const val ORIGINAL_ARTICLE_URL = "https://www.reuters.com/"
     const val BASE_URL = "https://justas.onboarding.lt/api/v2/"
 }
