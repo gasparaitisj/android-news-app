3b868246d7e00c4c956fe9f4906e6e06b17a9fab
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-11 08:26:05 +0300

    Add NewsSourceEntity.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/utils/data/AppDatabase.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/utils/data/AppDatabase.kt
@@ -4,9 +4,13 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [Article::class],
+    entities = [
+        ArticleEntity::class,
+        NewsSourceEntity::class
+    ],
     version = 1
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun articleDao(): ArticleDao
+    abstract fun newsSourceDao(): NewsSourceDao
 }
