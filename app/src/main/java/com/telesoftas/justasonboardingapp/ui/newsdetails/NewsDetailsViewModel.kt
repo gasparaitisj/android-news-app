209add9457dc1e82e0dca893051a479cf5a2d119
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-13 14:08:52 +0300

    Make getArticle() private in NewsDetailsViewModel.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/ui/newsdetails/NewsDetailsViewModel.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/ui/newsdetails/NewsDetailsViewModel.kt
@@ -39,7 +39,7 @@ class NewsDetailsViewModel @Inject constructor(
         }
     }
 
-    fun getArticle() {
+    private fun getArticle() {
         viewModelScope.launch {
             _article.value = Resource.loading()
             val response = articlesRepository.getArticleById(id)
