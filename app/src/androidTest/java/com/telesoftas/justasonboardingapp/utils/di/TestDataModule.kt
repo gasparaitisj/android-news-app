f8d4bf7eb9af120866761365aaeaec83923a175d
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-13 10:12:25 +0300

    Add TestDataModule.kt

---
+++ app/src/androidTest/java/com/telesoftas/justasonboardingapp/utils/di/TestDataModule.kt
@@ -0,0 +1,44 @@
+package com.telesoftas.justasonboardingapp.utils.di
+
+import android.content.Context
+import androidx.room.Room
+import com.telesoftas.justasonboardingapp.utils.data.AppDatabase
+import com.telesoftas.justasonboardingapp.utils.data.ArticleDao
+import com.telesoftas.justasonboardingapp.utils.data.NewsSourceDao
+import com.telesoftas.justasonboardingapp.utils.preferences.PreferencesStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [DataModule::class]
+)
+object TestDataModule {
+    @Singleton
+    @Provides
+    fun providePrefsStore(@ApplicationContext context: Context): PreferencesStore {
+        return PreferencesStore(context)
+    }
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext applicationContext: Context): AppDatabase {
+        return Room.inMemoryDatabaseBuilder(applicationContext, AppDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideArticleDao(database: AppDatabase): ArticleDao = database.articleDao()
+
+    @Singleton
+    @Provides
+    fun provideNewsSourceDao(database: AppDatabase): NewsSourceDao = database.newsSourceDao()
+
+}
