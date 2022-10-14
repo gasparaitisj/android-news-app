fa102a6e6e1b3da6ceaebba181c83e9b326616fc
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-14 14:06:55 +0300

    Modify Text in FavoriteScreen.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/ui/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/ui/favorite/FavoriteScreen.kt
@@ -117,7 +117,7 @@ fun FavoriteScreenContent(
                                     contentDescription = "Empty favorites image"
                                 )
                                 Text(
-                                    text = "You don't have any favorite articles yet",
+                                    text = "You don't have any favorite articles yet.",
                                     style = Typography.body2
                                 )
                             }
