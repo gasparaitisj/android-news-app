0e0dc021f4eb6d22ba4a021c3096128e141bb107
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-21 08:57:06 +0300

    Update MapScreen.kt with custom cluster manager

--- app/src/main/java/com/telesoftas/justasonboardingapp/ui/map/MapScreen.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/ui/map/MapScreen.kt
@@ -1,15 +1,31 @@
 package com.telesoftas.justasonboardingapp.ui.map
 
+import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.clustering.ClusterManager
+import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.compose.*
+import com.telesoftas.justasonboardingapp.R
+import com.telesoftas.justasonboardingapp.ui.theme.Typography
 
 @MapsComposeExperimentalApi
 @Composable
@@ -17,11 +33,10 @@ fun MapScreen(
     navController: NavHostController,
     viewModel: MapViewModel = hiltViewModel()
 ) {
-    val defaultCameraPosition = CameraPosition.fromLatLngZoom(
-        LatLng(55.92930340811748, 23.306731553438404), 11f
-    )
+    val locations = viewModel.locations
+    val defaultCameraPosition = viewModel.defaultCameraPosition
     GoogleMapClustering(
-        items = viewModel.locations,
+        items = locations,
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = rememberCameraPositionState { position = defaultCameraPosition }
     )
@@ -34,19 +49,100 @@ fun GoogleMapClustering(
     modifier: Modifier,
     cameraPositionState: CameraPositionState
 ) {
+    var clusterManager by remember { mutableStateOf<LocationClusterManager?>(null) }
     val context = LocalContext.current
-    var clusterManager by remember { mutableStateOf<ClusterManager<LocationItem>?>(null) }
+    val viewGroup = LocalView.current as ViewGroup
+    val compositionContext = rememberCompositionContext()
+
     GoogleMap(
         modifier = modifier,
         cameraPositionState = cameraPositionState
     ) {
         MapEffect(items) { map ->
-            clusterManager = ClusterManager<LocationItem>(context, map)
-            clusterManager?.addItems(items)
+            clusterManager = LocationClusterManager(
+                context = context,
+                viewGroup = viewGroup,
+                compositionContext = compositionContext,
+                map = map,
+                items = items,
+                infoWindowContent = { marker ->
+                    InfoWindow(marker)
+                }
+            )
         }
         LaunchedEffect(key1 = cameraPositionState.isMoving) {
             if (!cameraPositionState.isMoving) {
                 clusterManager?.onCameraIdle()
+            }
+        }
+    }
+}
+
+@Composable
+fun InfoWindow(marker: Marker) {
+    val uriHandler = LocalUriHandler.current
+    Surface(
+        elevation = 3.dp,
+        shape = RoundedCornerShape(24.dp),
+        color = Color.White.copy(alpha = 0.8f)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img_gintarine_logo),
+                contentDescription = "Gintarinė vaistinė"
+            )
+            Text(
+                modifier = Modifier.padding(4.dp),
+                text = marker.title ?: "",
+                style = Typography.subtitle1,
+                color = Color.Black
+            )
+            Text(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clickable { marker.snippet?.let { uriHandler.openUri(it) } },
+                text = marker.snippet ?: "",
+                style = Typography.subtitle1,
+                color = Color.Black
+            )
+            Text(
+                modifier = Modifier.padding(4.dp),
+                text = String.format(
+                    "%.5f, %.5f",
+                    marker.position.latitude, marker.position.longitude
+                ),
+                style = Typography.subtitle2,
+                color = Color.Black
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InfoWindowPreview() {
+    Surface(
+        shape = RoundedCornerShape(24.dp),
+        color = Color.White
+    ) {
+        Column {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "Gintarine vaistine",
+                    style = Typography.subtitle1
+                )
+                Text(
+                    text = "https://gintarine.lt",
+                    style = Typography.subtitle1
+                )
+                Text(
+                    text = "-64.2341234678; 36.123412346",
+                    style = Typography.subtitle2
+                )
             }
         }
     }
