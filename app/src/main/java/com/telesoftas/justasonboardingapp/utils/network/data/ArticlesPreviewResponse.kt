76beaea265efd2daee0068c46ec0dee53f6e29ab
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-31 15:19:05 +0200

    Reformat ArticlesPreviewResponse.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/utils/network/data/ArticlesPreviewResponse.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/utils/network/data/ArticlesPreviewResponse.kt
@@ -2,58 +2,58 @@ package com.telesoftas.justasonboardingapp.utils.network.data
 
 import com.squareup.moshi.Json
 
-data class ArticlePreviewResponse (
-    @Json(name="id")
+data class ArticlePreviewResponse(
+    @Json(name = "id")
     val id: String,
 
-    @Json(name="votes")
+    @Json(name = "votes")
     val votes: Long,
 
-    @Json(name="publishedAt")
+    @Json(name = "publishedAt")
     val publishedAt: String,
 
-    @Json(name="source")
+    @Json(name = "source")
     val source: String? = null,
 
-    @Json(name="category")
+    @Json(name = "category")
     val category: ArticleCategory,
 
-    @Json(name="author")
+    @Json(name = "author")
     val author: String? = null,
 
-    @Json(name="title")
+    @Json(name = "title")
     val title: String? = null,
 
-    @Json(name="description")
+    @Json(name = "description")
     val description: String? = null,
 
-    @Json(name="imageUrl")
+    @Json(name = "imageUrl")
     val imageUrl: String? = null
 )
 
 enum class ArticleCategory(val value: String) {
-    @Json(name="Politics")
+    @Json(name = "Politics")
     POLITICS("Politics"),
 
-    @Json(name="Gaming")
+    @Json(name = "Gaming")
     GAMING("Gaming"),
 
-    @Json(name="Sports")
+    @Json(name = "Sports")
     SPORTS("Sports"),
 
-    @Json(name="Culture")
+    @Json(name = "Culture")
     CULTURE("Culture"),
 
-    @Json(name="Food")
+    @Json(name = "Food")
     FOOD("Food"),
 
-    @Json(name="Business")
+    @Json(name = "Business")
     BUSINESS("Business"),
 
-    @Json(name="Health")
+    @Json(name = "Health")
     HEALTH("Health"),
 
-    @Json(name="Other")
+    @Json(name = "Other")
     OTHER("Other"),
 
     NONE("")
