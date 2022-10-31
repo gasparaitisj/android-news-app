bd9c8b7a727d6fbd3963a479b90cdc4537f7c1fd
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-10-31 14:48:54 +0200

    Fix typo in OnboardingPage.kt

--- app/src/main/java/com/telesoftas/justasonboardingapp/ui/tutorial/OnboardingPage.kt
+++ app/src/main/java/com/telesoftas/justasonboardingapp/ui/tutorial/OnboardingPage.kt
@@ -4,24 +4,24 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.telesoftas.justasonboardingapp.R
 
-sealed class OnBoardingPage(
+sealed class OnboardingPage(
     @DrawableRes val imageResId: Int,
     @StringRes val titleResId: Int,
     @StringRes val descriptionResId: Int
 ) {
-    object First : OnBoardingPage(
+    object First : OnboardingPage(
         imageResId = R.drawable.tutorial_first_image,
         titleResId = R.string.tutorial_screen_first_title,
         descriptionResId = R.string.tutorial_screen_first_description
     )
 
-    object Second : OnBoardingPage(
+    object Second : OnboardingPage(
         imageResId = R.drawable.tutorial_second_image,
         titleResId = R.string.tutorial_screen_second_title,
         descriptionResId = R.string.tutorial_screen_second_description
     )
 
-    object Third : OnBoardingPage(
+    object Third : OnboardingPage(
         imageResId = R.drawable.tutorial_third_image,
         titleResId = R.string.tutorial_screen_third_title,
         descriptionResId = R.string.tutorial_screen_third_description
