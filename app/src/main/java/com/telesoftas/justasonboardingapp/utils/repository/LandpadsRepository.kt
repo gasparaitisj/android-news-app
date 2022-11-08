a215bd212426c87c3c20f32751ab3aabfaf393c6
Justas Gasparaitis <justas.gasparaitis@telesoftas.com>
2022-11-08 14:49:03 +0200

    Add LandpadsRepository.kt

---
+++ app/src/main/java/com/telesoftas/justasonboardingapp/utils/repository/LandpadsRepository.kt
@@ -0,0 +1,35 @@
+package com.telesoftas.justasonboardingapp.utils.repository
+
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.await
+import com.telesoftas.justasonboardingapp.LandpadsQuery
+import com.telesoftas.justasonboardingapp.utils.network.Resource
+import com.telesoftas.justasonboardingapp.utils.network.data.Landpad
+import com.telesoftas.justasonboardingapp.utils.network.data.Location
+import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class LandpadsRepository @Inject constructor(
+    private val apolloClient: ApolloClient
+) {
+    suspend fun getLandpads(): Resource<List<Landpad>> {
+        return try {
+            Resource.success(
+                apolloClient.query(LandpadsQuery()).await().data?.landpads?.map { landpad ->
+                    Landpad(
+                        location = Location(landpad?.location?.latitude, landpad?.location?.longitude),
+                        fullName = landpad?.full_name,
+                        successfulLandings = landpad?.successful_landings,
+                        attemptedLandings = landpad?.attempted_landings,
+                        wikipedia = landpad?.wikipedia
+                    )
+                } ?: listOf()
+            )
+        } catch (exception: Exception) {
+            Timber.e(exception.message)
+            Resource.error(exception.message)
+        }
+    }
+}
